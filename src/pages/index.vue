e6731710dceadf74b13080fcf0ca0557d98390c1
Mineros04 <adam.jaros04@gmail.com>
2025-12-21 18:01:06 +0100

    feat: redirect to culling page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
+import { useRouter } from "vue-router";
+
 import { Icon } from "@iconify/vue";
+import { invoke } from "@tauri-apps/api/core";
 import { open } from "@tauri-apps/plugin-dialog";
 
-// eslint-disable-next-line
+const router = useRouter();
+
 async function selectDirectory() {
   const dir = await open({
     directory: true,
@@ -16,26 +20,26 @@ async function selectDirectory() {
   }
 
   // Directory provided, pass it to backend to parse and prepare state.
+  await invoke("init_images", { dirStr: dir });
+  router.push("/cull");
 }
 </script>
 
 <template>
-  <div>
-    <img
-      src="@/assets/bg-1.avif"
-      alt=""
-      class="fixed top-0 left-0 -z-1 h-screen w-screen object-cover"
-    />
-    <div class="grid h-screen w-screen place-items-center">
-      <div
-        class="card bg-base-300 shadow-glow-card flex flex-col items-center gap-4 rounded-4xl p-16"
-      >
-        <h1 class="card-title text-4xl font-black tracking-wide">KullFlow</h1>
-        <span> Begin by choosing a directory to cull. </span>
-        <router-link class="btn btn-info mt-4 text-xl" to="/cull">
-          <Icon icon="mdi:folder-open" /> <span>Choose a directory</span>
-        </router-link>
-      </div>
+  <img
+    src="@/assets/bg-1.avif"
+    alt=""
+    class="fixed top-0 left-0 -z-1 h-screen w-screen object-cover"
+  />
+  <div class="grid h-screen w-screen place-items-center">
+    <div
+      class="card bg-base-300 shadow-glow-card flex flex-col items-center gap-4 rounded-4xl p-16"
+    >
+      <h1 class="card-title text-4xl font-black tracking-wide">KullFlow</h1>
+      <span>Begin by choosing a directory to cull.</span>
+      <button class="btn btn-info mt-4 text-xl" @click="selectDirectory">
+        <Icon icon="mdi:folder-open" /> <span>Choose a directory</span>
+      </button>
     </div>
   </div>
 </template>
